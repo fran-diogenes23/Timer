934fe43ba9012f1e726d04a98a4eec68106bd8ed
Francisco Diogenes <59796440+fran-diogenes23@users.noreply.github.com>
2023-10-03 14:31:06 -0400

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+function getTimeFromSeconds(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC'
+    });
+}
+
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+let segundos = 0;
+let timer;
+
+function iniciaRelogio() {
+    timer = setInterval(function () {
+        segundos++;
+        relogio.innerHTML = getTimeFromSeconds(segundos);
+    }, 1000);
+}
+
+iniciar.addEventListener('click', function (event) {
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    iniciaRelogio();
+});
+pausar.addEventListener('click', function (event) {
+    relogio.classList.add('pausado');
+    clearInterval(timer);
+});
+zerar.addEventListener('click', function (event) {
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    segundos = 0;
+    relogio.innerHTML = '00:00:00';
+});
